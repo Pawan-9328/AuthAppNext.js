ab8ea9dd7fa02bc83d7436e5025717da83f48b77
Pawan Gola <pawangola908@gmail.com>
2024-04-11 04:46:29 +0530

    Login Frontend page completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,88 @@
-import React from 'react'
+"use client";
 
-function page() {
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      // loading state change like signup button disabled
+      setLoading(true);
+      //for data fetch
+      //user - here all data store
+      const response = await axios.post("/api/users/login", user);
+      //show this msg on browser screen
+      console.log("Login success", response.data);
+      // home route some but home router k aage url post fix ho jta hai
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login Failed");
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 
+    ) {
+      //user signup allow
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Processing" : "Login"}</h1>
+      <hr />
 
-export default page
+      {/* email  */}
+
+      <label htmlFor="email">email</label>
+      <input
+        id="email"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        // value is imp++ [detect ho rh h state se ]
+        value={user.email}
+        // user destructuring(spread) [change only one value - username]
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      {/* password */}
+
+      <label htmlFor="password">password</label>
+      <input
+        id="password"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        // value is imp++ [detect ho rh h state se ]
+        value={user.password}
+        // user destructuring(spread) [change only one value - username]
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        {buttonDisabled ? "No Login" : "Login"}
+      </button>
+      <Link href="/signup">Visit Signup page</Link>
+    </div>
+  );
+}
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,15 +1,72 @@
-'use client'
+"use client";
 
-import React, {useState} from 'react'
-
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
 export default function VerifyEmailPage() {
-   const [token, setToken] = useState("");
-   const [verified, setverified] = useState(false)
-   const [error, setError] = useState(false)
+  // utiliazation of next.js
+  // fetch data from the url
+  //const router = useRouter();
 
- 
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifiedUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyEmail", { token });
+      // user verified
+      setVerified(true);
+      // after verified error exits
+      setError(false)
+    } catch (error: any) {
+      setError(true);
+      console.log(error.response.data);
+    }
+  };
+
+  // as soon component mount ho then run
+  useEffect(() => {
+   setError(false)
+    // token niklna
+    const urlToken = window.location.search.split("=")[1];
+    // now your value urlToken else empty
+    setToken(urlToken || "");
+
+    //extract the query
+    //const  query  = router;
+    // out the token from the query
+   // const urlTokenTwo = query.token;
+  }, []);
+
+  //for any changes in token
+  useEffect(() => {
+   setError(false)
+    if (token.length > 0) {
+      verifiedUserEmail();
+    }
+  }, [token]);
 
   return (
-    <div></div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Verify Email</h1>
+      <h2 className="p-2 bg-orange-500 text-black">
+        {token ? `${token}` : "no token"}
+      </h2>
+      {verified && (
+        <div>
+          <h2>Verified</h2>
+          <Link href="/login">Login</Link>
+        </div>
+      )}
+
+      {error && (
+        <div>
+          <h2>Error</h2>
+          
+        </div>
+      )}
+    </div>
+  );
 }
